b82ee6cd92b133151fae6db57cd2e5f4b10b7a3c
zhangchenghao <2282610045@qq.com>
2024-10-19 00:07:04 +0800

    get employee by id

--- src/main/java/com/example/reggie/controller/EmployeeController.java
+++ src/main/java/com/example/reggie/controller/EmployeeController.java
@@ -156,4 +156,12 @@ public class EmployeeController {
         employeeService.updateById(employee);
         return R.success("员工信息修改成功");
     }
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable Long id){
+        Employee employee = employeeService.getById(id);
+        if (employee != null){
+            return R.success(employee);
+        }
+        return R.error("没有查询到对应员工信息");
+    }
 }
